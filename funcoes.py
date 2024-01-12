cf9ff3ea704d37cb6f0e5fd7a7f4755e4f74ab86
Auridebson Lima <auridebson@vutus.com>
2024-01-12 11:20:10 -0300

    Update

--- funcoes.py
+++ funcoes.py
@@ -34,3 +34,23 @@ def conta_palavras(texto:str):
         if letra == " ":
             controle = controle + 1
     return controle+1
+
+def ln(x):
+    print("-"*x)
+
+
+def soma(num1:int, num2:int):
+    resultado = num1 + num2
+    return resultado
+
+def subtracao(num1, num2):
+    resultado = num1 - num2
+    return resultado
+
+def divisao(num1,num2):
+    resultado = num1 / num2
+    return resultado
+
+def multiplicacao(num1, num2):
+    resultado = num1 * num2
+    return resultado
--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from funcoes import *
-from random import *
 
-entrada = pass_lower(input("Digite o seu texto: "))
+ln(30)
 
 
-print(conta_palavras(entrada))
 
+while True:
+    menu = int(input("""
+    Escolha a operação que deseja fazer:
+        [1] - Somar
+        [2] - Subtrair
+        [3] - Dividir
+        [4] - Multiplicar
+        [0] - Sair
+    """))
+    match menu:
+        case 1:
+            ln(10)
+            num1 = int(input("Digite o primeiro número: "))
+            num2 = int(input("Digite o segundo número: "))
+            ln(15)
+            print(f"Resultado: {soma(num1, num2)}")
+            ln(15)
+        case 2:
+            print("Subtração escolhida")
+        case 3:
+            print("Divisão escolhida")
+        case 4:
+            print("Multiplicação escolhida")
+        case 0:
+            print("Saindo...")
+            break
