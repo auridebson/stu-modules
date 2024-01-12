5020a9266ff4cee5529e4be8f26c183372fdbbe1
Auridebson Lima <auridebson@vutus.com>
2024-01-12 10:23:04 -0300

    Update

---
+++ funcoes.py
@@ -0,0 +1,36 @@
+vogais = ["a","e","i","o","u","á","é","í","ó","ú","à"]
+
+def pass_upper(texto:str):
+    return texto.upper()
+
+def pass_lower(texto:str):
+    return texto.lower()
+
+def conta_vogais(texto:str):
+    controle = 0
+    for letra in texto:
+        if letra in vogais:
+            controle = controle + 1
+    return controle
+
+def conta_consoantes(texto:str):
+    controle = 0
+    for letra in texto:
+        if letra in vogais:
+            continue
+        else:
+            controle = controle + 1
+    return controle
+
+def conta_caracteres(texto:str):
+    controle = 0
+    for letra in texto:
+        controle = controle + 1
+    return controle
+
+def conta_palavras(texto:str):
+    controle = 0
+    for letra in texto:
+        if letra == " ":
+            controle = controle + 1
+    return controle+1
